76a3989a52f38f9ef3ea1e97d73a1f5cbdc2f29a
StephenFoster-UAGC <stephen.foster2@student.uagc.edu>
2026-08-23 20:20:27 -0700

    Week 3 updates
    Added localStorage, TMDB API integration, Cart and Store pages.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
     The University of Arizona Global Campus
     INT499: Capstone for Information Technology
     Professor Amine Dehmani
-    August 9, 2026
+    August 10, 2026
 */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
@@ -12,9 +12,76 @@ import Home from "./pages/Home";
 import Movies from "./pages/Movies";
 import Cart from "./pages/Cart";
 import About from "./pages/About";
+import { useEffect, useState } from "react";
+import Shop from "./components/Shop";
 import "./App.css";
 
 function App() {
+  
+  const [cart, setCart] = useState(() => {
+  const savedCart = localStorage.getItem("streamListCart");
+
+  return savedCart
+    ? JSON.parse(savedCart)
+    : [];
+});
+useEffect(() => {
+  localStorage.setItem(
+    "streamListCart",
+    JSON.stringify(cart)
+  );
+}, [cart]);
+  const handleChange = (item, change) => {
+    setCart((currentCart) =>
+      currentCart.map((cartItem) =>
+        cartItem.id === item.id
+          ? {
+              ...cartItem,
+              amount: Math.max(
+                1,
+                cartItem.amount + change
+              )
+            }
+          : cartItem
+      )
+    );
+  };
+  const handleClick = (item) => {
+  const isSubscription = item.id <= 4;
+
+  const subscriptionInCart = cart.some(
+    (cartItem) => cartItem.id <= 4
+  );
+
+  const itemExists = cart.some(
+    (cartItem) => cartItem.id === item.id
+  );
+
+  // Only one subscription of any type is allowed
+  if (isSubscription && subscriptionInCart) {
+    alert(
+      "Only one subscription plan can be added to your cart at a time."
+    );
+    return;
+  }
+
+  // Prevent duplicate merchandise
+  if (itemExists) {
+    alert(
+      "This item is already in your cart. You can adjust the quantity in the Cart."
+    );
+    return;
+  }
+
+  setCart([
+    ...cart,
+    {
+      ...item,
+      amount: 1
+    }
+  ]);
+};
+
   return (
     <BrowserRouter>
       <div className="app">
@@ -24,8 +91,25 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/movies" element={<Movies />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route
+              path="/cart"
+              element={
+                <Cart
+                  cart={cart}
+                  setCart={setCart}
+                  handleChange={handleChange}
+              />
+            }
+          />
             <Route path="/about" element={<About />} />
+            <Route
+              path="/shop"
+              element={
+                <Shop
+                  handleClick={handleClick}
+              />
+            }
+          />
           </Routes>
         </main>
 
